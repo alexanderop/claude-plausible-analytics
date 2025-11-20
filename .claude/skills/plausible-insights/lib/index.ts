bdbc712effe7659d9679f0be02455d7ac61c20b9
Alexander Opalic <opalic@gmx.net>
2025-11-20 17:56:27 +0100

    feat: add index exports for public API

---
+++ .claude/skills/plausible-insights/lib/index.ts
@@ -0,0 +1,9 @@
+// Export all public APIs
+export * from './client/schemas.js';
+export * from './client/errors.js';
+export * from './client/plausible.js';
+export * from './queries/basic.js';
+export * from './queries/seo.js';
+export * from './utils/cache.js';
+export * from './utils/logger.js';
+export * from './utils/date-ranges.js';
